7a773e02c5090f8b120f0c987732ca6d46e2acc9
IN22915210 <mdsadiq.hussain@tesco.com>
2020-09-07 12:24:20 +0530

    add env variable
    Signed-off-by: IN22915210 <mdsadiq.hussain@tesco.com>

--- src/main/java/com/azure/app/services/azureapp/resources/HelloWorldResource.java
+++ src/main/java/com/azure/app/services/azureapp/resources/HelloWorldResource.java
@@ -1,5 +1,6 @@
 package com.azure.app.services.azureapp.resources;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,10 +8,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloWorldResource {
 
+	@Value("${environment}")
+	private String environment;
+
 	@GetMapping("/{name}")
 	public String getHello(@PathVariable("name") String name) {
-		return "Hello " + name;
+		return "Hello " + name + " " + environment;
 
 	}
-	
+
 }
